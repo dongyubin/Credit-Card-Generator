829a743633b16a72faeb6e5dd4c5335666c6b888
itxh <892457803@qq.com>
2025-07-04 10:42:04 +0800

    2025年7月4日：更新faqs

--- app/GoogleAdsence.tsx
+++ app/GoogleAdsence.tsx
@@ -5,31 +5,37 @@ import Script from "next/script";
 const GoogleAdsence = () => {
   return (
     <>
-      {process.env.NEXT_PUBLIC_GOOGLE_ADSENCE ? (
+      {process.env.NEXT_PUBLIC_GOOGLE_ADSENCE && (
         <>
+          {/* 1. 加载 Google AdSense 库 */}
           <Script
-            id="google-adsence"
+            id="google-adsense-library"
+            strategy="afterInteractive"
+            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENCE}`}
+            crossOrigin="anonymous"
+          />
+
+          {/* 2. 放置广告容器 */}
+          <ins
+            className="adsbygoogle"
+            style={{ display: 'block' }}
+            data-ad-client={process.env.NEXT_PUBLIC_GOOGLE_ADSENCE}
+            data-ad-slot="1430942601"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          />
+
+          {/* 3. 初始化广告 */}
+          <Script
+            id="google-adsense-init"
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `
-              <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENCE}"
-     crossorigin="anonymous"></script>
-                    <ins class="adsbygoogle"
-                    style="display:block"
-                    data-ad-client="${process.env.NEXT_PUBLIC_GOOGLE_ADSENCE}"
-                    data-ad-slot="1430942601"
-                    data-ad-format="auto"
-                    data-full-width-responsive="true"></ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
-              })();
-            `,
+                (adsbygoogle = window.adsbygoogle || []).push({});
+              `
             }}
           />
         </>
-      ) : (
-        <></>
       )}
     </>
   );
