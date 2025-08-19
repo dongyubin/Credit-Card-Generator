7bdb91a8c89917f27061134e7bb3265af1155bdc
DongYubin <892457803@qq.com>
2025-08-19 17:21:26 +0800

    优化SEO内容

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next-sitemap').IConfig} */
-
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://starter.weijunext.com",
+  siteUrl: process.env.SITE_URL || 'https://creditcardgenerator.wangdu.site',
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  exclude: [
+    '/server-sitemap.xml',
+    '/404',
+    '/admin',
+    '/admin/*'
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    additionalSitemaps: [
+      `${process.env.SITE_URL || 'https://creditcardgenerator.wangdu.site'}/sitemap.xml`,
+    ],
+  },
+  // 移除 additionalPaths 函数
 };
